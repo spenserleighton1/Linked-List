3ea8aaa47f6fb49983373c3f9c3861b17562b47e
Spenser Leighton <spenser.leighton@gmail.com>
2018-03-24 11:40:30 -0600

    Initial commit

---
+++ javaScript.js
@@ -0,0 +1,9 @@
+var enterButton = document.querySelector("#enter");
+var websiteTitle = document.querySelector('#website-title');
+var websiteUrl = document.querySelector('#website-url');
+
+enterButton.addEventListener('click', function() {
+  event.preventDefault(); 
+      console.log("enter button working");
+      
+      });
