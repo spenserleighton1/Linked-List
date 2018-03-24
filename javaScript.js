f375f04595c8c78d1ef5a2b9e2444761b85d65f9
Spenser Leighton <spenser.leighton@gmail.com>
2018-03-24 12:50:01 -0600

    Define variables

--- javaScript.js
+++ javaScript.js
@@ -1,9 +1,15 @@
 var enterButton = document.querySelector("#enter");
 var websiteTitle = document.querySelector('#website-title');
 var websiteUrl = document.querySelector('#website-url');
+var websiteLink = document.querySelector('#websiteLink')
 
 enterButton.addEventListener('click', function() {
   event.preventDefault(); 
       console.log("enter button working");
-      
+      websiteLink.innerText = websiteUrl ;
       });
+
+
+if (websiteTite = "") {
+document.querySelector("boolmarks").style.display = "none";
+}
