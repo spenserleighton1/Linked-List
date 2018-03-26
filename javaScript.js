a3fd19f03a900f407a9691291e55bab98a0745e1
Alex Bruce <brucealmightyalex@yahoo.com>
2018-03-26 14:25:08 -0600

    CSS formatting

--- javaScript.js
+++ javaScript.js
@@ -1,4 +1,38 @@
-var enterButton = document.querySelector('#enter');
+var enterButton = document.querySelector("#enter");
 var websiteTitle = document.querySelector('#website-title');
-var websiteUrl = document.querySelector('#website-url');
-var
+var websiteUrl = document.querySelector('#url');
+var bookmarkTitle = document.querySelector('#bookmark')
+var websiteLink = document.querySelector('#websiteLink')
+
+      websiteLink.innerText = websiteUrl.value;
+      bookmarkTitle.innerText = websiteTitle.value;
+
+
+
+
+
+enterButton.addEventListener('click', function(){
+  var 
+})
+  
+
+
+document.getElementById("pleaseWork").innerHTML+= 
+    ` <li id="bookmarks">
+    <h1 id="bookmark" width="100%"></h1>
+    <p id="websiteLink"></p>
+    <button id="readButton">Read</button>
+    <button id="deleteButton">Delete</button>`
+      };
+
+
+function addBookmark(){
+      websiteLink.innerText = websiteUrl.value;
+      bookmarkTitle.innerText = websiteTitle.value;
+    document.getElementById("pleaseWork").innerHTML+= 
+    ` <li id="bookmarks">
+    <h1 id="bookmark" width="100%">   </h1>
+    <p id="websiteLink">   </p>
+    <button id="readButton">Read</button>
+    <button id="deleteButton">Delete</button>`
+};
