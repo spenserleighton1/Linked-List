58d5d31d98fcbfc39313085f7f6d654e8f98fc1c
Spenser Leighton <spenser.leighton@gmail.com>
2018-03-26 16:25:57 -0600

    Fixed link in JS

--- javaScript.js
+++ javaScript.js
@@ -1,15 +1,20 @@
 var enterButton = document.querySelector("#enter");
 var websiteTitle = document.querySelector('#website-title');
-var websiteUrl = document.querySelector('#website-url');
+var websiteUrl = document.querySelector('#url');
+var bookmarkTitle = document.querySelector('#bookmark')
 var websiteLink = document.querySelector('#websiteLink')
 
-enterButton.addEventListener('click', function() {
-  event.preventDefault(); 
-      console.log("enter button working");
-      websiteLink.innerText = websiteUrl ;
-      });
+     // websiteLink.innerText = websiteUrl.value;
+     // bookmarkTitle.innerText = websiteTitle.value;
 
 
-if (websiteTite = "") {
-document.querySelector("boolmarks").style.display = "none";
-}
+enterButton.onclick = function() {
+ event.preventDefault();
+
+document.getElementById("list").innerHTML+=
+   `<li id="bookmarks">
+   <h1 id="bookmark" width="100%">${websiteTitle.value}</h1>
+   <p id="websiteLink">${websiteUrl.value}</p>
+   <button id="readButton">Read</button>
+   <button id="deleteButton">Delete</button>`
+};
