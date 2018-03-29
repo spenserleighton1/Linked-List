c02f4f65c06d1cc0bf9ccffb94321f06ec82fb95
Spenser Leighton <spenser.leighton@gmail.com>
2018-03-29 07:28:39 -0600

    Fixed css styles and made responsive

--- javaScript.js
+++ javaScript.js
@@ -5,15 +5,6 @@ var bookmarkTitle = document.querySelector('#bookmark')
 var websiteLink = document.querySelector('#websiteLink')
 var cardCount = 0;
 
-
-// document.querySelector('ul').addEventListener('click', function(event) {
-//   if (event.target.tagName.deleteCard() === 'article') {
-//     console.log('yes')
-//     // do your action on your 'li' or whatever it is you're listening for
-//   }
-// })
-
-
 enterButton.onclick = function(e) {
  e.preventDefault();
  if (websiteTitle.value === "" || websiteUrl.value === "") {
@@ -22,27 +13,25 @@ enterButton.onclick = function(e) {
  } else {
   document.querySelector("#list").innerHTML +=
    
-       `<article class="bookmarks">
-       <li class="unread">
-       <h1 class="bookmark-h1" width="100%">${websiteTitle.value}</h1>
-       <a id="websiteLink" href="${websiteUrl.value}">${websiteUrl.value}</a>
-       <button class="read" id="readButton" onclick="markCardRead(${cardCount})">Read</button>
-       <button class="delete" id="deleteButton" onclick="deleteCard(${cardCount})">Delete</button>
-       </li>
-       </article>`
-
+  `<article class="bookmarks">
+   <li class="unread">
+   <h1 class="websiteTitle" width="100%">${websiteTitle.value}</h1>
+   <a class="websiteLink" href="${websiteUrl.value}">${websiteUrl.value}</a>
+   <button class="read" id="readButton" onclick="markCardRead(${cardCount})">Read</button>
+   <button  id="deleteButton" onclick="deleteCard(${cardCount})">Delete</button>
+   </li>
+   </article>`
   };
+  
   cardCount++;
 
 document.querySelector("form").reset();
 document.querySelector('.counter').innerText = cardCount;
 };
-//figure out how to target specific cardId and delete that shit!
 
 function markCardRead(cardId){
   var bookmarkCard = document.querySelector('.bookmarks',)
   bookmarkCard.classList.toggle('bookmarksRead');
-  // console.log(cardId, " card clicked");
 };
 
 function deleteCard(cardId){
@@ -50,5 +39,4 @@ function deleteCard(cardId){
   document.querySelector('.counter').innerText = cardCount;
   var bookmarkCard = document.querySelector('.bookmarks');
   bookmarkCard.className ='delete';
-  console.log(cardId, " card delete");
 };
